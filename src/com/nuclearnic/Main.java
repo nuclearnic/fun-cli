7600a2656477a3333fdacd6367c620ccc15b3fe6
nuclearnic <nuclearnicdev@gmail.com>
2018-09-15 21:11:25 +0200

    read and print

--- src/com/nuclearnic/Main.java
+++ src/com/nuclearnic/Main.java
@@ -1,8 +1,18 @@
 package com.nuclearnic;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("nuclearCLI");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What type of fun are you into? \n 1: Super Fun\"");
+
+        String x = sc.next();
+        System.out.println(String.format("%s", x));
+
+
+
     }
 }
